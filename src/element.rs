bce1668850e5e4cb932da0f05a91cdd186a58d00
Alan Jeffrey <ajeffrey@mozilla.com>
2019-11-05 13:05:37 -0600

    Add debugging category

--- src/element.rs
+++ src/element.rs
@@ -6,18 +6,41 @@ use glib::subclass::types::ObjectSubclass;
 use gstreamer::subclass::element::ElementClassSubclassExt;
 use gstreamer::subclass::element::ElementImpl;
 use gstreamer::subclass::ElementInstanceStruct;
+use gstreamer::Caps;
+use gstreamer::DebugCategory;
+use gstreamer::DebugColorFlags;
+use gstreamer::Fraction;
+use gstreamer::FractionRange;
+use gstreamer::IntRange;
+use gstreamer::List;
+use gstreamer::PadDirection;
+use gstreamer::PadPresence;
+use gstreamer::PadTemplate;
 use gstreamer_base::subclass::base_transform::BaseTransformClassSubclassExt;
 use gstreamer_base::subclass::base_transform::BaseTransformImpl;
 use gstreamer_base::subclass::BaseTransformMode::NeverInPlace;
 use gstreamer_base::BaseTransform;
+use gstreamer_video::VideoFormat;
 
-pub struct MyElement {}
+pub struct MyElement {
+    cat: DebugCategory,
+}
 
 impl ObjectSubclass for MyElement {
     const NAME: &'static str = "MyElement";
     type ParentType = BaseTransform;
     type Instance = ElementInstanceStruct<Self>;
     type Class = ClassStruct<Self>;
+
+    fn new() -> Self {
+        Self {
+            cat: DebugCategory::new(
+                "myelement",
+                DebugColorFlags::empty(),
+                Some("My element by me"),
+            ),
+        }
+    }
 
     fn class_init(klass: &mut ClassStruct<Self>) {
         klass.set_metadata(
