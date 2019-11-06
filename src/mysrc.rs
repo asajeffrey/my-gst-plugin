5f9967e34b8ca332c97b200c6e06d3d408a462c9
Alan Jeffrey <ajeffrey@mozilla.com>
2019-11-06 15:21:28 -0600

    Initialize MySrc

--- src/mysrc.rs
+++ src/mysrc.rs
@@ -3,10 +3,19 @@ use glib::glib_object_subclass;
 use glib::subclass::object::ObjectImpl;
 use glib::subclass::simple::ClassStruct;
 use glib::subclass::types::ObjectSubclass;
+use gstreamer::subclass::element::ElementClassSubclassExt;
 use gstreamer::subclass::element::ElementImpl;
 use gstreamer::subclass::ElementInstanceStruct;
+use gstreamer::Caps;
+use gstreamer::Fraction;
+use gstreamer::FractionRange;
+use gstreamer::IntRange;
+use gstreamer::PadDirection;
+use gstreamer::PadPresence;
+use gstreamer::PadTemplate;
 use gstreamer_base::subclass::base_src::BaseSrcImpl;
 use gstreamer_base::BaseSrc;
+use gstreamer_video::VideoFormat;
 
 pub struct MySrc {}
 
@@ -18,6 +27,31 @@ impl ObjectSubclass for MySrc {
 
     fn new() -> Self {
         Self {}
+    }
+
+    fn class_init(klass: &mut ClassStruct<Self>) {
+        klass.set_metadata(
+            "My Src By Me",
+            "Filter/Effect/Converter/Video",
+            "Does stuff",
+            env!("CARGO_PKG_AUTHORS"),
+        );
+
+        let sink_caps = Caps::new_simple(
+            "video/x-raw",
+            &[
+                ("format", &VideoFormat::Bgrx.to_string()),
+                ("width", &IntRange::<i32>::new(0, std::i32::MAX)),
+                ("height", &IntRange::<i32>::new(0, std::i32::MAX)),
+                (
+                    "framerate",
+                    &FractionRange::new(Fraction::new(0, 1), Fraction::new(std::i32::MAX, 1)),
+                ),
+            ],
+        );
+        let sink_pad_template =
+            PadTemplate::new("sink", PadDirection::Sink, PadPresence::Always, &sink_caps).unwrap();
+        klass.add_pad_template(sink_pad_template);
     }
 
     glib_object_subclass!();
